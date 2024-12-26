7dbebbc969a79a2afcb6eb73d3a0023af26014f2
Brainiac Rawkib <brainiacrawkib@gmail.com>
2024-12-26 17:20:38 +0100

    updates

---
+++ ch4/railApiGin/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/http"
+	"rail_api_gin/dbutils"
+)
+
+// DB Driver visible to whole program
+var DB *sql.DB
+
+// StationResource holds information about locations
+type StationResource struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	OpeningTime string `json:"opening_time"`
+	ClosingTime string `json:"closing_time"`
+}
+
+// GetStation returns the station details
+func GetStation(c *gin.Context) {
+	var station StationResource
+	id := c.Param("station_id")
+	err := DB.QueryRow("select ID, NAME, CAST(OPENING_TIME as CHAR), CAST(CLOSING_TIME as CHAR) from station where id=?", id).
+		Scan(&station.ID, &station.OpeningTime, &station.ClosingTime)
+
+	if err != nil {
+		log.Println(err)
+		c.JSON(600, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"results": station,
+		})
+	}
+}
+
+// GetStation returns the station details
+func GetStations(c *gin.Context) {
+	var station StationResource
+	err := DB.QueryRow("select * from station").
+		Scan(&station.ID, &station.OpeningTime, &station.ClosingTime)
+
+	if err != nil {
+		log.Println(err)
+		c.JSON(600, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"results": station,
+		})
+	}
+}
+
+// CreateStation handles the POST
+func CreateStation(c *gin.Context) {
+	var station StationResource
+	// Parse the body into our resource
+	if err := c.BindJSON(&station); err == nil {
+		// Format Time to Go time format
+		statement, _ := DB.Prepare("insert into station (NAME, OPENING_TIME, CLOSING_TIME) values (?, ?, ?)")
+		result, _ := statement.Exec(station.Name, station.OpeningTime, station.ClosingTime)
+		if err == nil {
+			newID, _ := result.LastInsertId()
+			station.ID = int(newID)
+			c.JSON(http.StatusOK, gin.H{
+				"result": station,
+			})
+		} else {
+			c.String(http.StatusInternalServerError, err.Error())
+		}
+	} else {
+		c.String(http.StatusInternalServerError, err.Error())
+	}
+}
+
+// RemoveStation handles the deletion of resource
+func RemoveStation(c *gin.Context) {
+	id := c.Param("station-id")
+	statement, _ := DB.Prepare("delete from station where id=?")
+	_, err := statement.Exec(id)
+	if err != nil {
+		log.Println(err)
+		c.JSON(600, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.String(http.StatusOK, "")
+	}
+}
+
+func main() {
+	var err error
+	DB, err = sql.Open("sqlite3", "./railapiGin.db")
+	if err != nil {
+		log.Println("Driver creation failed!")
+	}
+	dbutils.Initialize(DB)
+	r := gin.Default()
+	// Add routes to REST verbs
+	prefixRoute := "/v1"
+	r.GET(prefixRoute+"/stations/:station_id", GetStation)
+	r.POST(prefixRoute+"/stations", CreateStation)
+	r.DELETE(prefixRoute+"/stations/:station_id", RemoveStation)
+	r.Run(":8000") // Default listen and serve on 0.0.0.0:8080
+}
